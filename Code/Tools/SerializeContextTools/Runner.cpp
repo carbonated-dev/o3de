8f9e62b20400899d1510e4d6e7bc4216ff0c6a7d
VladimirLagutin <vlagutin@tech-round.com>
2023-05-11 21:42:26 +0300

    Revert "Revert "Material Patcher""
    This reverts commit daba5eaa06682c5f5c4f13adc56fe236a76a6af4.

--- Code/Tools/SerializeContextTools/Runner.cpp
+++ Code/Tools/SerializeContextTools/Runner.cpp
@@ -161,6 +161,11 @@ namespace SerializeContextTools
             {
                 result = Dumper::CreateType(application);
             }
+            else if (AZ::StringFunc::Equal("patch-materials", action))
+            {
+                SliceConverter sliceConverter;
+                result = sliceConverter.PatchMaterials(application);
+            }
             else
             {
                 commandExecuted = false;
