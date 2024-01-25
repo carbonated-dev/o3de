f1850b7b5986d6390fa9f9dcf14f5df57a277953
moudgils <47460854+moudgils@users.noreply.github.com>
2024-01-25 11:26:32 -0800

    Addressed feedback
    Signed-off-by: moudgils <47460854+moudgils@users.noreply.github.com>

--- Gems/Atom/Feature/Common/Code/Include/Atom/Feature/CoreLights/LightCommon.h
+++ Gems/Atom/Feature/Common/Code/Include/Atom/Feature/CoreLights/LightCommon.h
@@ -22,19 +22,32 @@ namespace AZ::Render::LightCommon
     }
 
     // Check if a view is being used by a pipeline that has a GPU culling pass.
+    // @param parentScene - Parent Scene associated with a feature processor
+    // @param view - Check if gpu culling passes are part of this view
+    // @param gpuCullingData - Cached Unordered set of Render Pipelines and views that will contain the LightCullingPass which can be used for querying purposes
     bool HasGPUCulling(
         RPI::Scene* parentScene,
         const RPI::ViewPtr& view,
-        AZStd::unordered_set<AZStd::pair<const RPI::RenderPipeline*, const RPI::View*>>& gpuCullingPasses);
+        AZStd::unordered_set<AZStd::pair<const RPI::RenderPipeline*, const RPI::View*>>& gpuCullingData);
 
-    // Update gpuCullingData to hold information related to gpu culling passes in order to Check if render pipeline is using GPU culling
+    //! Update gpuCullingData to hold information related to gpu culling passes in order to Check if render pipeline is using GPU culling
+    //! @param renderPipeline - Cache data associated with the passed RenderPipeline
+    //! @param newView The new view triggered from view changes via OnRenderPipelinePersistentViewChanged
+    //! @param previousView The previous view triggered from view changes via OnRenderPipelinePersistentViewChanged
+    //! @param gpuCullingData - Update an Unordered set of Render Pipelines and views that will contain the LightCullingPass
      void CacheGPUCullingPipelineInfo(
         RPI::RenderPipeline* renderPipeline,
         RPI::ViewPtr newView,
         RPI::ViewPtr previousView,
         AZStd::unordered_set<AZStd::pair<const RPI::RenderPipeline*, const RPI::View*>>& gpuCullingData);
 
-     // Get or create a buffer that will be used for visibility when doing CPU culling.
+     //! Get or create a buffer that will be used for visibility when doing CPU culling.
+     //! This function will be called for multiple views and as a result will create new buffers if the demand increases
+     //! @param visibleBufferUsedCount - Number of visible objects cached for the gpu buffer
+     //! @param visibleBufferHandlers - Handle to gpu buffers
+     //! @param bufferName - Gpu Buffer name
+     //! @param bufferSrgName - Gpu buffer srg name
+     //! @param elementCountSrgName - Count SRG name related to the number of objects represented by the buffer
      GpuBufferHandler& GetOrCreateVisibleBuffer(
          uint32_t& visibleBufferUsedCount,
          AZStd::vector<GpuBufferHandler>& visibleBufferHandlers,
