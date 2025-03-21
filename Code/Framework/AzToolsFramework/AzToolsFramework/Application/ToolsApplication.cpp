e6b9031fd2d3ebe287d5800f9ceb15c1afc3669a
Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>
2025-03-21 12:51:20 -0700

    Fixes lua not initializing properties (WIP)
    Signed-off-by: Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>

--- Code/Framework/AzToolsFramework/AzToolsFramework/Application/ToolsApplication.cpp
+++ Code/Framework/AzToolsFramework/AzToolsFramework/Application/ToolsApplication.cpp
@@ -1261,7 +1261,13 @@ namespace AzToolsFramework
 
     UndoSystem::URSequencePoint* ToolsApplication::ResumeUndoBatch(UndoSystem::URSequencePoint* expected, const char* label)
     {
-        if (m_currentBatchUndo)
+        if ((!m_undoStack) || (!expected))
+        {
+            return BeginUndoBatch(label);
+        }
+
+        // if we are in an undo already, and its already the expected one, just return it.
+        if ((m_currentBatchUndo) && (m_currentBatchUndo == expected))
         {
             if (m_undoStack->GetTop() == m_currentBatchUndo)
             {
@@ -1271,16 +1277,13 @@ namespace AzToolsFramework
             return m_currentBatchUndo;
         }
 
-        if (m_undoStack)
+        const auto ptr = m_undoStack->GetTop();
+        if (ptr && ptr == expected)
         {
-            const auto ptr = m_undoStack->GetTop();
-            if (ptr && ptr == expected)
-            {
-                m_currentBatchUndo = ptr;
-                m_undoStack->PopTop();
+            m_currentBatchUndo = ptr;
+            m_undoStack->PopTop();
 
-                return m_currentBatchUndo;
-            }
+            return m_currentBatchUndo;
         }
 
         return BeginUndoBatch(label);
@@ -1312,6 +1315,10 @@ namespace AzToolsFramework
     void ToolsApplication::EndUndoBatch()
     {
         AZ_Assert(m_currentBatchUndo, "Cannot end batch - no batch current");
+        if (!m_currentBatchUndo)
+        {
+            return; // let's not crash just becuase there's a programmer error.
+        }
 
         if (m_currentBatchUndo->GetParent())
         {
