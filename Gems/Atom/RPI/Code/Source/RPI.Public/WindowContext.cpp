d94d0549780b589d8a730dd15ade4fcebc6907ce
VladimirLagutin <vlagutin@tech-round.com>
2025-08-28 16:34:50 +0300

    MAD-18558 Change error message

--- Gems/Atom/RPI/Code/Source/RPI.Public/WindowContext.cpp
+++ Gems/Atom/RPI/Code/Source/RPI.Public/WindowContext.cpp
@@ -185,7 +185,7 @@ namespace AZ
                 else // if (AZ::RHI::Factory::Get().GetAPIUniqueIndex() == static_cast<uint32_t>(AZ::RHI::APIIndex::Vulkan))
                 {
 #if !defined(AZ_PLATFORM_WINDOWS)
-                    AZ_Error("WindowContext::OnDesiredFPSChanged", false, "'desired_fps' is not investigated/implemented for Mac and Linux yet. Will be used 'Sleep' method like for Windows+Vulkan");
+                    AZ_Error("WindowContext::OnDesiredFPSChanged", false, "desired_fps command is not implemented for this platform yet");
 #endif
                     console->PerformCommand("vsync_interval 0");
                     console->PerformCommand(AZStd::string::format("sys_MaxFPS %i", desiredFPS).c_str());
