75080b7642782c01318e704096db62bba2005709
Alexander Shubin <ashubin@tech-round.com>
2025-10-24 19:29:59 +0300

    support for gradle plugin 8.9 has been added

--- scripts/o3de/o3de/android_support.py
+++ scripts/o3de/o3de/android_support.py
@@ -317,6 +317,12 @@ class AndroidGradlePluginRequirements(object):
 
 ANDROID_GRADLE_PLUGIN_COMPATIBILITY_MAP = {
 
+    '8.9': AndroidGradlePluginRequirements(agp_version='8.9.1',
+                                           gradle_version='8.11.1',
+                                           sdk_build_tools_version='35.0.0',
+                                           jdk_version='17',
+                                           release_note_url='https://developer.android.com/build/releases/past-releases/agp-8-9-0-release-notes'),
+
     '8.2': AndroidGradlePluginRequirements(agp_version='8.2',
                                            gradle_version='8.2',
                                            sdk_build_tools_version='35.0.0',
