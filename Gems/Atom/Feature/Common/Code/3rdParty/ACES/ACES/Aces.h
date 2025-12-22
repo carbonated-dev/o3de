72bb2f025c42ec726f8486924e302a6857206b41
Nikolay Lisun <nlisun@tech-round.com>
2025-12-22 14:18:31 +0300

    Add LUT in the cube file format

--- Gems/Atom/Feature/Common/Code/3rdParty/ACES/ACES/Aces.h
+++ Gems/Atom/Feature/Common/Code/3rdParty/ACES/ACES/Aces.h
@@ -140,7 +140,22 @@ namespace AZ
             float m_scale = 1.0f;
         };
 
-        AZ_ENUM_CLASS_WITH_UNDERLYING_TYPE(DisplayMapperOperationType, uint32_t,
+#if defined(CARBONATED) && defined(CARBONATED_LUT_TEXTURE)
+        AZ_ENUM_CLASS_WITH_UNDERLYING_TYPE(
+            DisplayMapperOperationType, uint32_t,
+            Aces,
+            AcesLut,
+            Passthrough,
+            GammaSRGB,
+            Reinhard,
+            AcesFitted,
+            AcesFilmic,
+            Filmic,
+            LUT
+        );
+#else
+        AZ_ENUM_CLASS_WITH_UNDERLYING_TYPE(
+            DisplayMapperOperationType, uint32_t,
             Aces,
             AcesLut,
             Passthrough,
@@ -150,6 +165,7 @@ namespace AZ
             AcesFilmic,
             Filmic
         );
+#endif
 
         enum class ShaperPresetType
         {
@@ -171,6 +187,9 @@ namespace AZ
             AcesFitted,
             AcesFilmic,
             Filmic
+#if defined(CARBONATED) && defined(CARBONATED_LUT_TEXTURE)
+            , LUT
+#endif
         };
 
         enum class TransferFunctionType
