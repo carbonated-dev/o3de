50574ff6eea12cd5c45e115c18db2d8b08b10c02
VladimirLagutin <vlagutin@tech-round.com>
2026-03-17 17:59:42 +0300

    Restoring ProjectActivity.java

--- Code/Tools/Android/ProjectBuilder/ProjectActivity.java
+++ Code/Tools/Android/ProjectBuilder/ProjectActivity.java
@@ -8,10 +8,7 @@
 package ${ANDROID_PACKAGE};
 
 import android.util.Log;
-import android.os.Bundle;
-
 import com.amazon.lumberyard.LumberyardActivity;
-import com.appsflyer.AppsFlyerLib;
 
 public class ${ANDROID_PROJECT_ACTIVITY} extends LumberyardActivity
 {
@@ -20,21 +17,5 @@ public class ${ANDROID_PROJECT_ACTIVITY} extends LumberyardActivity
         Log.d("LMBR", "BootStrap: Starting Library load");
         System.loadLibrary("c++_shared");
         Log.d("LMBR", "BootStrap: Finished Library load");
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-
-        AppsFlyerLib.getInstance().init("gj6kxeB8gEoMmAb4F7XnGP", null, this);
-        AppsFlyerLib.getInstance().start(this);
-    }
-
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        AppsFlyerLib.getInstance().start(this);
     }
 }
