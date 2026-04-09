e0a4e4cef9f0c8a751bdd1a30c4de448dbd968b1
Alexander Efimov <aefimov@tech-round.com>
2026-04-09 17:37:32 +0300

    Init SDL, pump events

--- Code/Framework/AzFramework/Platform/Common/SDL/AzFramework/SDLApplication.cpp
+++ Code/Framework/AzFramework/Platform/Common/SDL/AzFramework/SDLApplication.cpp
@@ -10,6 +10,9 @@
 #include <AzFramework/SDLEventHandler.h>
 #include <AzFramework/SDLInterface.h>
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+
 namespace AzFramework
 {
     ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -32,32 +35,76 @@ namespace AzFramework
     SDLApplication::SDLApplication()
     {
         LinuxLifecycleEvents::Bus::Handler::BusConnect();
+
+        /*
         m_sdlConnectionManager = AZStd::make_unique<SDLConnectionManagerImpl>();
         if (SDLConnectionManagerInterface::Get() == nullptr)
         {
             SDLConnectionManagerInterface::Register(m_sdlConnectionManager.get());
+        }
+        */
+
+        /*
+        SDL2
+        SDL_INIT_TIMER: timer subsystem
+        SDL_INIT_AUDIO: audio subsystem
+        SDL_INIT_VIDEO: video subsystem; automatically initializes the events subsystem
+        SDL_INIT_JOYSTICK: joystick subsystem; automatically initializes the events subsystem
+        SDL_INIT_HAPTIC: haptic (force feedback) subsystem
+        SDL_INIT_GAMECONTROLLER: controller subsystem; automatically initializes the joystick subsystem
+        SDL_INIT_EVENTS: events subsystem
+        SDL_INIT_EVERYTHING: all of the above subsystems
+        */
+        const uint32_t flags = SDL_INIT_EVENTS | SDL_INIT_VIDEO | SDL_INIT_AUDIO;
+        const int res = SDL_Init(flags);
+        if (!res)
+        {
+            const char* error = SDL_GetError();
+            AZ_Error("SDL", false, "Cannot initialize SDL with flags %x: return code %d, error '%s'", flags, res, error);
+            AZ_Assert(false, "Cannot initialize SDL");
         }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     SDLApplication::~SDLApplication()
     {
+        SDL_Quit();
+
+        /*
         if (SDLConnectionManagerInterface::Get() == m_sdlConnectionManager.get())
         {
             SDLConnectionManagerInterface::Unregister(m_sdlConnectionManager.get());
         }
         m_sdlConnectionManager.reset();
+        */
+
         LinuxLifecycleEvents::Bus::Handler::BusDisconnect();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     void SDLApplication::PumpSystemEventLoopOnce()
     {
+        SDL_PumpEvents();  // do we need it here?
+        SDL_Event event;
+        if (SDL_PollEvent(&event))
+        {
+            SDLEventHandlerBus::Broadcast(&SDLEventHandlerBus::Events::HandleSDLEvent, event);
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     void SDLApplication::PumpSystemEventLoopUntilEmpty()
     {
+        SDL_PumpEvents();  // do we need it here?
+        SDL_Event event;
+        for (;;)
+        {
+            if (!SDL_PollEvent(&event))
+            {
+                break;
+            }
+            SDLEventHandlerBus::Broadcast(&SDLEventHandlerBus::Events::HandleSDLEvent, event);
+        }
     }
 
 } // namespace AzFramework
