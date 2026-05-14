708546756aef810fcd1f59bc0ab0496b5cae2f7f
Alexander Efimov <aefimov@tech-round.com>
2026-05-14 21:27:38 +0300

    Fix thread metadata

--- Code/Framework/AzFramework/GridMate/Carrier/Carrier.cpp
+++ Code/Framework/AzFramework/GridMate/Carrier/Carrier.cpp
@@ -1320,7 +1320,8 @@ CarrierThread::CarrierThread(const CarrierDesc& desc, AZStd::shared_ptr<Compress
         {
             threadDesc.m_priority = desc.m_threadPriority;
         }
-        m_thread = AZStd::thread(AZStd::bind(&CarrierThread::ThreadPump, this), &threadDesc);
+        //m_thread = AZStd::thread(AZStd::bind(&CarrierThread::ThreadPump, this), &threadDesc);
+        m_thread = AZStd::thread(threadDesc, AZStd::bind(&CarrierThread::ThreadPump, this));
     }
     else
     {
