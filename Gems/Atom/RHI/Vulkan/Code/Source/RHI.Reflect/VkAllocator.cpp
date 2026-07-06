76ad3e1f89d45e16da0949ce1bb974f302d03edd
Akio Gaule <10719597+akioCL@users.noreply.github.com>
2026-07-06 19:32:20 -0400

    Add r_vkUseCustomAllocator CVAR to enable/disable Vulkan custom allocator

--- Gems/Atom/RHI/Vulkan/Code/Source/RHI.Reflect/VkAllocator.cpp
+++ Gems/Atom/RHI/Vulkan/Code/Source/RHI.Reflect/VkAllocator.cpp
@@ -8,7 +8,20 @@
 
 #include <Atom/RHI.Reflect/VkAllocator.h>
 #include <Atom/RHI.Profiler/GraphicsProfilerBus.h>
+#if defined(CARBONATED)
+#include <AzCore/Console/IConsole.h>
+#endif
 #include <AzCore/Memory/SystemAllocator.h>
+
+#if defined(CARBONATED)
+AZ_CVAR(
+    bool,
+    r_vkUseCustomAllocator,
+    true,
+    nullptr,
+    AZ::ConsoleFunctorFlags::NeedsReload,
+    "Enable Vulkan VkAllocationCallbacks backed by AZ::SystemAllocator. Must be set before Vulkan objects are created; changing it at runtime requires reload/restart.");
+#endif
 
 namespace AZ
 {
@@ -44,7 +57,11 @@ namespace AZ
             // For instance creation/destruction use nullptr for VkAllocationCallbacks* as using VkSystemAllocator::Get() crashes RenderDoc
             // when used with openxr enabled projects. We think it's because RenderDoc is maybe injecting something when doing allocations.
             // Because of this we deactivate callbacks when using Renderdoc.
+#if defined(CARBONATED)
+            if (r_vkUseCustomAllocator && !RHI::GraphicsProfilerBus::HasHandlers())
+#else
             if (!RHI::GraphicsProfilerBus::HasHandlers())
+#endif
             {
                 m_allocationCallbacks = AZStd::make_unique<VkAllocationCallbacks>();
                 m_allocationCallbacks->pfnFree = RHI_vkFreeFunction;
