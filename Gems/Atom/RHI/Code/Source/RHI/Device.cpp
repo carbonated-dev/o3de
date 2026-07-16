6833488e2af6e8d9a118ea447aa48e3f2018875e
Akio Gaule <10719597+akioCL@users.noreply.github.com>
2026-07-16 14:06:14 -0400

    Disable raytracing again

--- Gems/Atom/RHI/Code/Source/RHI/Device.cpp
+++ Gems/Atom/RHI/Code/Source/RHI/Device.cpp
@@ -23,7 +23,7 @@ namespace AZ::RHI
 
 #if defined(CARBONATED)
 
-    AZ_CVAR(bool, r_disableRayTracing, false, nullptr, AZ::ConsoleFunctorFlags::NeedsReload, "Disable Ray Tracing.");
+    AZ_CVAR(bool, r_disableRayTracing, true, nullptr, AZ::ConsoleFunctorFlags::NeedsReload, "Disable Ray Tracing. Disabled by default");
 
     const char* VendorIdToString(VendorId vendorId)
     {
